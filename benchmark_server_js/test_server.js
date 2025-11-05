307065173b7aa3f3a659315cd18e2832c8099cf5
Josef Prochazka <josef.prochazka@apify.com>
2025-11-05 11:55:38 +0100

    Change to 2 links per page to avoid bombing RQ during the benchmark test

--- benchmark_server_js/test_server.js
+++ benchmark_server_js/test_server.js
@@ -5,14 +5,14 @@ const PORT = 8080;
 
 const args = process.argv.slice(2);
 // The only argument controls to which depth level the links to next level are generated
-const DEPTH_LEVEL = parseInt(args[0]) || 10;
+const DEPTH_LEVEL = parseInt(args[0]) || 33;
 
 // Function to generate HTML response
 function generateHtmlResponse(path) {
     let links = '';
     if (path.length !== DEPTH_LEVEL) {
         // Generate links based on the level
-        links = Array.from({ length: 10 }, (_, i) => `<a href="${path}${i}">${path}${i}</a>`).join('\n');
+        links = Array.from({ length: 2 }, (_, i) => `<a href="${path}${i}">${path}${i}</a>`).join('\n');
     }
 
     return `
